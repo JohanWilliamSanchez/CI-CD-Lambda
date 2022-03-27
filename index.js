6a352e919d7d42fdc345bf16f089279a4ccb06db
William Sanchez <jwsanchez@indracompany.com>
2022-03-27 18:17:19 -0500

    proyecto inicial

---
+++ index.js
@@ -0,0 +1,33 @@
+var MongoClient = require('mongodb').MongoClient
+
+exports.handler = async (event) => {
+    const ddb_connection = process.env.DDB_CONECTION;
+    var client = await MongoClient.connect(
+        ddb_connection,
+        {
+          tlsCAFile: `rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+        }
+      );
+    
+    const database = process.env.DATABASE;
+    var db = client.db(database);
+
+    //Specify the collection to be used PB
+    var col = db.collection('sample-collection');
+
+    const result = await col.insertOne({ 'hello': 'Amazon DocumentDB' });
+
+    console.log("result", result);
+
+    const document = await col.findOne({ 'hello': 'Amazon DocumentDB' });
+
+    console.log("document", document);
+
+    client.close();
+
+    const response = {
+        statusCode: 200,
+        body: document,
+    };
+    return response;
+};
